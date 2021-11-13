1db2f2c35d339e2bf26191a56026f13188891e92
Ricky Hu <ricky9667@gmail.com>
2021-11-13 22:46:23 +0800

    add installation and execute steps

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Apriori Algorithm
 
-Practice algorithm and test with some datasets.
+Practice Apriori algorithm and test with some datasets.
+
+## How to run
+
+### Get Python
+
+This algorithm requires Python, first you have to install [here](https://www.python.org/downloads/) for Windows or run the following command for Linux/macOS:
+
+```shell=
+# macOS
+brew install python
+
+# Linux
+apt install python
+```
+
+> Note: macOS is required to install [Homebrew](https://brew.sh) before running the command above
+
+### Run Apriori
+
+There are some easy datasets saved in `./test_data.py`, to run them with Apriori simply run:
+
+```python=
+# import our example datasets
+from test_data import dataset1
+
+# get frequent itemsets by Apriori, you will get a list of itemsets during the process of the algorithm
+frequent_itemsets = find_frequent_itemsets(dataset=dataset1, min_support=2)
+
+# show results
+for itemset in itemsets:
+    print(itemset)
+```
+
--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from apriori import find_frequent_itemsets
 
 def main():
     print("dataset1 = " + str(dataset1))
-    result = find_frequent_itemsets(dataset=dataset1, min_support=2)
+    frequent_itemsets = find_frequent_itemsets(dataset=dataset1, min_support=2)
 
     # print("dataset2 = " + str(dataset2))
-    # result = find_frequent_itemsets(dataset=dataset2, min_support=2)
+    # frequent_itemsets = find_frequent_itemsets(dataset=dataset2, min_support=2)
 
     # print("dataset3 = " + str(dataset3))
-    # find_frequent_itemsets(dataset=dataset3, min_support=2)
+    # frequent_itemsets = find_frequent_itemsets(dataset=dataset3, min_support=2)
 
     # print("dataset4 = " + str(dataset4))
-    # find_frequent_itemsets(dataset=dataset4, min_support=2)
+    # frequent_itemsets = find_frequent_itemsets(dataset=dataset4, min_support=2)
 
-    for frequent_itemset in result:
+    for frequent_itemset in frequent_itemsets:
         print(frequent_itemset)
 
 
