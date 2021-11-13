645a30cf4b920e98233940a79615cb5f42bc7875
TU-Lin <trusaidlin@gmail.com>
2021-11-14 01:10:20 +0800

    Update apriori.py
    There exist some suggest changes.

--- apriori.py
+++ apriori.py
@@ -1,58 +1,58 @@
 def generate_base_set(dataset):
     base_set = set()
-    for itemset in dataset:
-        for item in itemset:
+    for item_set in dataset:
+        for item in item_set:
             if item not in base_set:
                 base_set.add(item)
     return [{item} for item in list(base_set)]
 
 
-def calculate_item_appear_times(dataset, itemsets):
+def calculate_item_appear_times(dataset, item_sets):
     appear_dict = []
-    for index, itemset in enumerate(itemsets):
+    for _ in item_sets:
         appear_dict.append(0)
 
     for subset in dataset:
-        for index, itemset in enumerate(itemsets):
-            if itemset.difference(subset) == set():
+        for index, item_set in enumerate(item_sets):
+            if item_set.difference(subset) == set():
                 appear_dict[index] += 1
 
     return appear_dict
 
 
-def filter_by_min_support(itemsets, appear_times, min_support):
-    for index in range(len(itemsets)-1, -1, -1):
+def filter_by_min_support(item_sets, appear_times, min_support):
+    for index in range(len(item_sets) - 1, -1, -1):
         if appear_times[index] < min_support:
-            itemsets.pop(index)
+            item_sets.pop(index)
             appear_times.pop(index)
-    return itemsets
+    return item_sets
 
 
-def generate_next_itemsets(filtered_itemsets):
+def generate_next_item_sets(filtered_item_sets):
     dataset = []
-    for i, itemset1 in enumerate(filtered_itemsets):
-        for j, itemset2 in enumerate(filtered_itemsets):
+    for i, item_set1 in enumerate(filtered_item_sets):
+        for j, item_set2 in enumerate(filtered_item_sets):
             if j <= i:
                 continue
-            if len(itemset1.difference(itemset2)) == 1:
-                new_itemset = itemset1.union(itemset2)
-                if new_itemset not in dataset:
-                    dataset.append(new_itemset)
+            if len(item_set1.difference(item_set2)) == 1:
+                new_item_set = item_set1.union(item_set2)
+                if new_item_set not in dataset:
+                    dataset.append(new_item_set)
     return dataset
 
 
-def apriori(dataset, itemsets, min_support):
-    appear_times = calculate_item_appear_times(dataset, itemsets)
-    filtered_itemsets = filter_by_min_support(itemsets, appear_times, min_support)
-    if len(filtered_itemsets) <= 1:
+def apriori(dataset, item_sets, min_support):
+    appear_times = calculate_item_appear_times(dataset, item_sets)
+    filtered_item_sets = filter_by_min_support(item_sets, appear_times, min_support)
+    if len(filtered_item_sets) <= 1:
         return []
 
-    next_dataset = generate_next_itemsets(filtered_itemsets)
-    frequent_itemsets = apriori(dataset, next_dataset, min_support)
-    frequent_itemsets.insert(0, filtered_itemsets)
-    return frequent_itemsets
+    next_dataset = generate_next_item_sets(filtered_item_sets)
+    frequent_item_sets = apriori(dataset, next_dataset, min_support)
+    frequent_item_sets.insert(0, filtered_item_sets)
+    return frequent_item_sets
 
 
-def find_frequent_itemsets(dataset, min_support):
-    base_itemset = generate_base_set(dataset)
-    return apriori(dataset, base_itemset, min_support)
+def find_frequent_item_sets(dataset, min_support):
+    base_item_set = generate_base_set(dataset)
+    return apriori(dataset, base_item_set, min_support)
