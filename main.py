983bc0d85e2238f3c5b1594c61492a0ce95c551a
Ricky Hu <ricky9667@gmail.com>
2021-11-15 13:29:36 +0800

    update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def main():
     #     print(frequent_itemset)
 
     dataset = generate_dataset_from_csv('datasets/my_movies.csv')
-    frequent_itemsets = find_frequent_itemsets(dataset=dataset[0:500], min_support=2)
+    frequent_itemsets = find_frequent_itemsets(dataset=dataset, min_support=2)
     for frequent_itemset in frequent_itemsets:
-        print(frequent_itemset)
+        print(str(frequent_itemset) + '\n')
 
 
 if __name__ == '__main__':
